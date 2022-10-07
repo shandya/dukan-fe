2e4dd5bcf71b7d3216347aad04c50b8f6209a69d
shandya-fas <113876129+shandya-fas@users.noreply.github.com>
2022-10-07 17:00:47 +0700

    update header site

--- dist/components/header.php
+++ dist/components/header.php
@@ -1,10 +1,136 @@
 <div class="header-information">
-  <a href="#" class="information-link">Conseils et Service</a>
-  <p class="information-copy">
+  <a href="#" class="information-link">
+    <span>Conseils et Service</span>
+  
+    <svg class="link-image">
+      <use xlink:href="assets/images/sprite.svg#svg-circular-arrow-right"></use>
+    </svg>
+  </a>
+  <p class="information-copy d-none d-md-block">
     This is for micro copy promos
   </p>
 
   <div class="language-selection-wrapper">
-    Anglais
+    <div class="dropdown dropdown-select-language">
+
+      <button class="btn dropdown-toggle" type="button" id="dropdownSelectLanguageButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        <svg class="dropdown-icon">
+          <use xlink:href="assets/images/sprite.svg#svg-globe"></use>
+        </svg>
+        <span>Anglais</span>
+        <svg class="button-image">
+          <use xlink:href="assets/images/sprite.svg#svg-chevron-right"></use>
+        </svg>
+
+      </button>
+      <div class="dropdown-menu dropdown-menu-right" aria-labelledby="dropdownSelectLanguageButton">
+        <a href="" class="dropdown-item">Belgique</a>
+        <a href="#" class="dropdown-item">Français</a>
+        <a href="#" class="dropdown-item">Allemagne</a>
+        <a href="#" class="dropdown-item">Pays-Bas</a>
+      </div>
+    </div>
   </div>
 </div>
+
+<nav class="navbar navbar-expand-lg site-header">
+  <div class="container container-navbar">
+    <div class="row">
+      <a class="navbar-brand" href="#">
+        <img src="assets/images/logo-dukan.png" alt="Dukan Boutique" class="navbar-brand-image">
+      </a>
+
+      <div class="header-promo-wrapper d-none d-md-flex">
+        <a href="" class="header-promo-link">
+          <svg class="header-promo-image">
+            <use xlink:href="assets/images/sprite.svg#svg-present"></use>
+          </svg>
+          <span>cadeau réservé aux nouveaux memebres</span>
+        </a>
+      </div>
+
+      <form class="form-inline my-2 my-lg-0 form-search-product d-none d-lg-flex">
+        <input class="form-control pl-3" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-search" type="submit">
+          <svg class="btn-icon">
+            <use xlink:href="assets/images/sprite.svg#svg-magnifying-glass"></use>
+          </svg>
+        </button>
+      </form>
+
+      <div class="navbar navbar-user-menu" id="navbarUserMenu">
+        <ul class="navbar-nav">
+          <li class="nav-item px-md-1">
+            <a class="nav-link" href="#">
+              <div class="shopping-wrapper wishlist-wrapper">
+                <svg class="shopping-icon">
+                  <use xlink:href="assets/images/sprite.svg#svg-favorite"></use>
+                </svg>
+                <span class="shopping-value">5</span>
+              </div>
+            </a>
+          </li>
+          <li class="nav-item px-md-1">
+            <a class="nav-link" href="#">
+              <div class="shopping-wrapper cart-wrapper">
+              <svg class="shopping-icon">
+                  <use xlink:href="assets/images/sprite.svg#svg-cart"></use>
+                </svg>
+                <span class="shopping-value">14</span>
+              </div>
+            </a>
+          </li>
+          <li class="nav-item">
+            <div class="current-basket">
+              <span class="current-basket-label">votre panier</span>
+              <div class="current-basket-value">€ 245.34</div>
+            </div>
+          </li>
+          <li class="nav-item px-1 d-none d-md-block">
+            <a class="nav-link btn btn-outline-primary px-2 px-md-3" href="#">se connecter</a>
+          </li>
+          <li class="nav-item px-1 d-none d-md-block">
+            <a class="nav-link btn btn-primary px-2 px-md-3" href="#">s'enregistrer</a>
+          </li>
+        </ul>
+      </div>
+
+
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarMenu" aria-expanded="false" aria-label="Toggle navigation">
+        toggle drop down
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    </div>
+
+    <div class="row">
+      <div class="collapse navbar-collapse navbar-menu" id="navbarMenu">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="#">Boutique</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Nouveautés</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Actualités</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Recettes</a>
+          </li>
+          <li class="nav-item nav-item--download-app">
+            <div class="download-app-wrapper">
+              <a href="" class="app-link px-1">
+                <img src="assets/images/logo-download-appstore.png" alt="" class="app-image">
+              </a>
+              <a href="" class="app-link px-1">
+                <img src="assets/images/logo-download-googleplay.png" alt="" class="app-image">
+              </a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</nav>
+
+                         
--- dist/index.php
+++ dist/index.php
@@ -300,6 +300,9 @@
 
     <script src="assets/js/jquery-3.6.0.min.js"></script>
     <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    
     <script type="module" src="assets/js/main.js"></script>
     <script type="module" src="assets/js/landing.js"></script>
   </body>
